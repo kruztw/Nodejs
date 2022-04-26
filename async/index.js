d0833733cb675ff6b3bcdabf5d1dafb8a4a7759b
raybai <raybai@synology.com>
2022-04-26 09:53:23 +0800

    update async/

--- async/index.js
+++ async/index.js
@@ -1,31 +1,51 @@
 // https://wcc723.github.io/development/2020/02/16/all-new-promise/
 
-function sub_yes() {
-  return new Promise((ok, not_ok) => {
+function foo11() {
+  return new Promise((resolve, reject) => {
       console.log("first");
-      setTimeout(function () { console.log("third"); ok("return this if success"); }, 1000); // 必須執行 ok 或 not_ok 才會返回
+      setTimeout(function () { console.log("third"); resolve("return this if success"); }, 1000); // 必須執行 resolve 或 reject 才會返回
       console.log("second");            // Promise 內部無法保證循序執行
   });
 }
 
-async function yes1() {
-  let result = await sub_yes();                       // 正確: 先進去才 wait (sub_yes 的 setTimeout)
+async function foo1() {
+  let result = await foo11();                       // 正確: 先進去才 wait (foo11 的 setTimeout)
   console.log(result);
   console.log("forth");
 }
 
+/* async awiat in promise */
+async function foo222() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () { console.log("second"); resolve("return this if success"); }, 1000);
+    });
+}
 
+function foo22() {
+    return new Promise(async (resolve, reject) => {
+        console.log("first");
+        await foo222();
+        console.log("third");
+        resolve("return this if success");
+  });
+}
+
+async function foo2() {
+  let result = await foo22();                       // 正確: 先進去才 wait
+  console.log(result);
+  console.log("forth");
+}
 
 /*
 錯誤示範
 */
 
-function sub_no() {
+function foo33() {
     console.log("first");     // 必須用 promise
 }
 
-async function no1() {
-    await setTimeout(sub_no, 1000);               // 錯誤: 先 wait 才進去, 在 wait 過程中 second 就已經執行了
+async function foo3() {
+    await setTimeout(foo33, 1000);                  // 錯誤: 先 wait 才進去, 在 wait 過程中 second 就已經執行了
     console.log("second");
 }
 
@@ -41,7 +61,8 @@ async function embed() {
     await console.log('d');
 }
 
-//yes1();
-//no1();
+//foo1();
+//foo2();
+//foo3();
 embed();
 
