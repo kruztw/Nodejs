bf21780c232b1206652d96e8f7e7f04829e90311
kruztw <kruztw@gmail.com>
2022-11-01 23:28:00 +0800

    add basic/variable/

---
+++ basic/variable/index.js
@@ -0,0 +1,7 @@
+let var1 = 1
+let var2 = null || "otherwise"
+let var3 = "something" || "otherwise"
+
+console.log("var1 = ", var1, typeof var1)
+console.log("var2 = ", var2, typeof var2)
+console.log("var3 = ", var3, typeof var3, var3.length)
